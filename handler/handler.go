e614aee574e77ddfa4e41ba13f5bcd3db6f64b6c
nafisaalfiani <nafisa.alfiani.ica@gmail.com>
2024-01-05 11:04:13 +0700

    fix

--- handler/handler.go
+++ handler/handler.go
@@ -32,7 +32,7 @@ func Init(config *config.Value, repo *repository.Repository, validator *validato
 }
 
 func (h *Handler) Ping(c echo.Context) error {
-	httpSuccess(c, http.StatusOK, "PONG!")
+	return httpSuccess(c, http.StatusOK, "PONG!")
 }
 
 // httpError is helper function for error response
